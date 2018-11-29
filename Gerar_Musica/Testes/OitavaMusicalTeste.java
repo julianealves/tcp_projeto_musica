78e6899f01a74d6285b2829c3f8285fe7ed8b70f
Juliane Alves <julianedarochaalves@gmail.com>
2018-11-29 19:50:52 -0200

    Refatoracao de codigo. Fix bug: oitava nao pode passar do valor 10 para algumas notas musicais

--- Gerar_Musica/Testes/OitavaMusicalTeste.java
+++ Gerar_Musica/Testes/OitavaMusicalTeste.java
@@ -8,53 +8,56 @@ public class OitavaMusicalTeste {
 
 	@Test
 	public void test() {
-		OitavaMusical OitavaMusical = new OitavaMusical(5);
-		assertEquals(5, OitavaMusical.getOitavaMusical());
+		OitavaMusical OitavaMusical_teste1 = new OitavaMusical(5);
+		assertEquals(5, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical.decrementaOitava();
-		assertEquals(4, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(4, OitavaMusical_teste1.getOitavaMusical());
 	
-		OitavaMusical.decrementaOitava();
-		assertEquals(3, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(3, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical.decrementaOitava();
-		assertEquals(2, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(2, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical.decrementaOitava();
-		assertEquals(1, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(1, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical.decrementaOitava();
-		assertEquals(0, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(0, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical.decrementaOitava();
-		assertEquals(0, OitavaMusical.getOitavaMusical());
+		OitavaMusical_teste1.decrementaOitava();
+		assertEquals(0, OitavaMusical_teste1.getOitavaMusical());
 		
-		OitavaMusical OitavaMusical2 = new OitavaMusical(5);
-		assertEquals(5, OitavaMusical2.getOitavaMusical());
+		OitavaMusical OitavaMusical_teste2 = new OitavaMusical(5);
+		assertEquals(5, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(6, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(6, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(7, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(7, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(8, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(8, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(9, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(9, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(10, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(10, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical2.incrementaOitava();
-		assertEquals(5, OitavaMusical2.getOitavaMusical());
+		OitavaMusical_teste2.incrementaOitava();
+		assertEquals(5, OitavaMusical_teste2.getOitavaMusical());
 		
-		OitavaMusical OitavaMusical3 = new OitavaMusical(-1);
-		assertEquals(0, OitavaMusical3.getOitavaMusical());
+		OitavaMusical OitavaMusical_teste3 = new OitavaMusical(-1);
+		assertEquals(0, OitavaMusical_teste3.getOitavaMusical());
 		
-		OitavaMusical OitavaMusical4 = new OitavaMusical(11);
-		assertEquals(10, OitavaMusical4.getOitavaMusical());
+		OitavaMusical OitavaMusical_teste4 = new OitavaMusical(11);
+		assertEquals(10, OitavaMusical_teste4.getOitavaMusical());
+		
+		OitavaMusical_teste4.setOitavaDefault();
+		assertEquals(5, OitavaMusical_teste4.getOitavaMusical());
 	}
 
 }
--- Gerar_Musica/Testes/VolumeTeste.java
+++ Gerar_Musica/Testes/VolumeTeste.java
@@ -8,40 +8,40 @@ public class VolumeTeste {
 
 	@Test
 	public void test() {
-		Volume volume = new Volume(17000);
-		assertEquals(16383, volume.getVolume());
+		Volume volume_teste1 = new Volume(17000);
+		assertEquals(16383, volume_teste1.getVolume());
 		
-		volume.setVolume(2000);
-		assertEquals(2000, volume.getVolume());
+		volume_teste1.setVolume(2000);
+		assertEquals(2000, volume_teste1.getVolume());
 		
-		volume.multiplicaVolume((float) 2.0);
-		assertEquals(2000*2, volume.getVolume());
+		volume_teste1.multiplicaVolume((float) 2.0);
+		assertEquals(2000*2, volume_teste1.getVolume());
 		
-		volume.multiplicaVolume((float) 5.0);
-		assertEquals(16383, volume.getVolume());
+		volume_teste1.multiplicaVolume((float) 5.0);
+		assertEquals(16383, volume_teste1.getVolume());
 		
-		volume.divideVolume((float) 4.0);
-		assertEquals(16383/4, volume.getVolume());
+		volume_teste1.divideVolume((float) 4.0);
+		assertEquals(16383/4, volume_teste1.getVolume());
 		
-		volume.divideVolume((float) 8.0);
-		assertEquals((16383/4)/8, volume.getVolume());
+		volume_teste1.divideVolume((float) 8.0);
+		assertEquals((16383/4)/8, volume_teste1.getVolume());
 		
-		volume.setVolume(20000);
-		assertEquals(16383, volume.getVolume());
+		volume_teste1.setVolume(20000);
+		assertEquals(16383, volume_teste1.getVolume());
 		
-		volume.setVolume(-1);
-		assertEquals(0, volume.getVolume());
+		volume_teste1.setVolume(-1);
+		assertEquals(0, volume_teste1.getVolume());
 		
-		volume.setVolume(16383);
-		volume.divideVolume((float) -4.0);
-		assertEquals(16383, volume.getVolume());
+		volume_teste1.setVolume(16383);
+		volume_teste1.divideVolume((float) -4.0);
+		assertEquals(16383, volume_teste1.getVolume());
 		
-		volume.setVolume(16383);
-		volume.multiplicaVolume((float) -4.0);
-		assertEquals(16383, volume.getVolume());
+		volume_teste1.setVolume(16383);
+		volume_teste1.multiplicaVolume((float) -4.0);
+		assertEquals(16383, volume_teste1.getVolume());
 		
-		Volume volume2 = new Volume(-5);
-		assertEquals(0, volume2.getVolume());
+		Volume volume_teste2 = new Volume(-5);
+		assertEquals(0, volume_teste2.getVolume());
 		
 	}
 
